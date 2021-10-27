6ca68cab0ca3e2940e91cbd96c7eacfb477b72d2
MysteriousAcadia <anshumansingh64@gmail.com>
2021-10-27 23:59:44 +0530

    Add evm provider

--- reef-frontend/src/App.js
+++ reef-frontend/src/App.js
@@ -2,24 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import AppContext from './Components/AppContext';
 import { web3Accounts, web3Enable, } from "@polkadot/extension-dapp"
+import { Provider, Signer as EvmSigner } from '@reef-defi/evm-provider';
+import { WsProvider } from '@polkadot/rpc-provider';
+
 import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+
 import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
 
 function App() {
+  const URL = 'wss://rpc-testnet.reefscan.com/ws';
+
   const [accounts, setAccounts] = useState();
+  const [evmProvider, setEvmProvider] = useState();
+  const [isApiConnected, setIsApiConnected] = useState();
   const extensionSetup = async () => {
     const extensions = await web3Enable('Wallet-connect-tutorial');
+    const newEvmProvider = new Provider({
+      provider: new WsProvider(URL)
+    });
+    setEvmProvider(newEvmProvider);
+    newEvmProvider.api.on('connected', () => setIsApiConnected(true));
+    newEvmProvider.api.on('disconnected', () => setIsApiConnected(false));
+    newEvmProvider.api.on('ready', async () => {
+      const accounts = await web3Accounts();
+      setAccounts(accounts);
+
+    })
     if (extensions.length === 0) {
       console.log('No extension installed!');
       return;
     }
-    const accounts = await web3Accounts();
-    setAccounts(accounts);
+
   };
   useEffect(() => {
     extensionSetup()
   }, []);
+
+
+
+
+
+
   return (
     <>
       <Navbar />
