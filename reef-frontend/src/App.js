cd010ba5521f105e4ad08ec7fad5b54325d3772b
akashjha011 <akash.jsmith@gmail.com>
2021-10-29 00:54:16 +0530

    Signer added

--- reef-frontend/src/App.js
+++ reef-frontend/src/App.js
@@ -2,11 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import AppContext from './Components/AppContext';
 import { web3Accounts, web3Enable, } from "@polkadot/extension-dapp"
-import { Provider, Signer as EvmSigner } from '@reef-defi/evm-provider';
+import { Provider, Signer} from '@reef-defi/evm-provider';
 import { WsProvider } from '@polkadot/rpc-provider';
-
 import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
+import { ethers,Contract } from 'ethers';
 
 import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
@@ -14,41 +13,74 @@ import Navbar from './Components/Navbar';
 function App() {
   const URL = 'wss://rpc-testnet.reefscan.com/ws';
 
-  const [accounts, setAccounts] = useState();
+  const [account, setAccounts] = useState();
   const [evmProvider, setEvmProvider] = useState();
   const [isApiConnected, setIsApiConnected] = useState();
+  const [signer,setSigner] = useState();
+
+
   const extensionSetup = async () => {
-    const extensions = await web3Enable('Wallet-connect-tutorial');
+
+    let allInjected = await web3Enable('Reef Marketplace');
+
+    if (allInjected.length === 0) {
+      console.log('No extension installed!');
+      return;
+    }
+
+    console.log(allInjected);
+
+    let injected;
+    if (allInjected[0] && allInjected[0].signer) {
+    injected = allInjected[0].signer;
+    }
+    
     const newEvmProvider = new Provider({
-      provider: new WsProvider(URL)
+      newEvmProvider: new WsProvider(URL)
     });
     setEvmProvider(newEvmProvider);
     newEvmProvider.api.on('connected', () => setIsApiConnected(true));
     newEvmProvider.api.on('disconnected', () => setIsApiConnected(false));
     newEvmProvider.api.on('ready', async () => {
-      const accounts = await web3Accounts();
-      console.log(accounts);
-      setAccounts(accounts);
+      const allAccounts = await web3Accounts();
+  
+      if (allAccounts[0] && allAccounts[0].address) {
+        console.log(allAccounts);
+        setAccounts(allAccounts[0].address);
+      }
+      
+      const wallet = new Signer(newEvmProvider, account, injected);
+      // Claim default account
+      if (!(await wallet.isClaimed())) {
+        console.log(
+          "No claimed EVM account found -> claimed default EVM account: ",
+          await wallet.getAddress()
+        );
+        await wallet.claimDefaultAccount();
+      }
 
+      setSigner(signer);
     })
-    if (extensions.length === 0) {
-      console.log('No extension installed!');
-      return;
-    }
+    
 
   };
   useEffect(() => {
     extensionSetup()
   }, []);
 
-
-
+  //contract address : 0x667F2F7A8F634117C2F187f1A2a06c44ADD84acC (test storage contract)
+  async function retrieveStorage(){
+    // const storageContract = new Contract("0x667F2F7A8F634117C2F187f1A2a06c44ADD84acC",abi,signer);
+    // const result = await storageContract.retrieve();
+    console.log(signer.getAddress())
+  }
 
 
 
   return (
     <>
       <Navbar />
+      <button type="button" className="button" onClick={retrieveStorage}>Get Storage</button> 
       <Home />
     </>
     // <div className="App">
