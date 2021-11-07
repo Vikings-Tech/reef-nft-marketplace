57debe5b849f52dde667fdf66a3337b6597fe14d
akashjha011 <akash.jsmith@gmail.com>
2021-11-07 22:03:36 +0530

    auction data update

--- reef-frontend/src/config/contractAddress.js
+++ reef-frontend/src/config/contractAddress.js
@@ -1,5 +1,5 @@
 exports.
     factoryContractAddress = "0xefE7bad65Ab1A85F736ebC91EC441ee4e027eE34";
 
-exports.nftMarketplaceAddress = "0x294384bC63e544DC3978A1F3f4Ac161D9A2fde68";
+exports.nftMarketplaceAddress = "0x1E4141ec01b16E65BBa54672d8F469dA56f2Cf65";
 
